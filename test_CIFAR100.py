3c345e7c4abc0d9b45d68123dce0051e53513d21
xyyzh <zxinyi1215@gmail.com>
2021-12-11 11:30:53 -0500

    created CIFAR train and test files

---
+++ test_CIFAR100.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 11:30:23 2021
+
+@author: xinyi
+"""
+
---
+++ train_CIFAR100.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 11:29:50 2021
+
+@author: xinyi
+"""
+
